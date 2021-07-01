9f202d936ffc34e90c9f82d577b2d7d75ade37d8
Anshuman Kumar <anshumank2512@gmail.com>
2021-07-01 10:07:02 +0530

    Stable code

--- email script/script.py
+++ email script/script.py
@@ -1,20 +1,27 @@
 import smtplib
+import os
+import pandas as pd
 from email.message import EmailMessage
-msg=EmailMessage()
+
+#establishing connection
+smtp= smtplib.SMTP_SSL('smtp.gmail.com',465)
+smtp.login("your email address","password")
+
+#Importing email list from CSV file
+email_list=pd.read_csv(os.path.join((os.getcwd), 'emails.csv'))
+emails=email_list['EMAIL']
+
+msg=EmailMessage()    
+msg.set_content('Body of the  mail')
 msg['Subject']="Subject of the mail"
 msg['From']="Sender's email address"
-msg['To']="Receiver's email address"
-msg.set_content('Body of the  mail')
+
 #Add file to be attached
 file='file.pdf'
 with open(file,'rb') as f:
     file_data=f.read()
     file_name=f.name
 msg.add_attachment(file_data, maintype='application', subtype='octet-stream',filename=file_name)
-with smtplib.SMTP_SSL('smtp.gmail.com',465) as smtp:
-    
-
-    smtp.login("your email address","password")
-
-    
-    smtp.send_message(msg)
+for contact in emails:
+   msg['To']=contact
+   smtp.send_message(msg)
